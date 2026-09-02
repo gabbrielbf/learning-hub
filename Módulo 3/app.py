a2c5d9f9f890031209de1aa07bcbd96ebf6560d1
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-02 18:57:03 -0400

    definindo primeira rota de inserção do dado na API e criando request com o retorno da "data"

--- Módulo 3/app.py
+++ Módulo 3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from models.task import Task
 
 app = Flask(__name__)
@@ -6,6 +6,12 @@ app = Flask(__name__)
 # CRUD -> Create, read, update, delete.
 
 tasks = []
+
+# Rota responsável por postar ou "inserir" um dado dentro da API
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json() # <- Recupera o que o cliente enviou pra a gente
+    return 
 
 if __name__ == '__main__':
     app.run(debug=True)
