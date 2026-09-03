3da27082bf6c97f7193e3ec23973ca4ee42dc211
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-03 11:39:40 -0400

    lógica de controle do ID nas tasks

--- Módulo 3/app.py
+++ Módulo 3/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # CRUD -> Create, read, update, delete.
 
 tasks = []
+task_id_control = 1
 
 # Rota responsável por postar ou "inserir" um dado dentro da API
 @app.route('/tasks', methods=['POST'])
@@ -13,7 +14,8 @@ def create_task():
     data = request.get_json() # <- Recupera o que o cliente enviou pra a gente
     # A nova task criada abaixo será derivada da nossa classe Task sua descrição tem dois elementos no data.get pois queremos que
     # seu valor padrão seja vazio
-    new_task = Task(title=data['title'], description=data.get('description', ''))
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ''))
+    task_id_control += 1 # lógica para definir que os IDs não se repitam, pois cada um deve ser unico
     return 
 
 if __name__ == '__main__':
