9d31a77ba577044f41300482088f486781d6b54e
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-04 17:52:15 -0400

    lógica de teste de retorno da resposta do servidor == 200 concluida

--- Módulo 3/tests.py
+++ Módulo 3/tests.py
@@ -1,1 +1,18 @@
 # Arquivo designado a testes relacionados a nossa pequena API
+import pytest, requests
+
+# CRUD -> Create, read, update, delete.
+# Testes abaixo se basearão letra a letra com uma URL fixa
+
+BASE_URL = 'http://127.0.0.1:5000'
+tasks = [] # <- Adicionando tarefas criadas para testarmos as demais atividades com os endpoints 'RUD'.
+
+# Create
+def test_create_task():
+
+    new_task_data = {
+        'title': 'New task',
+        'description': 'Description of new task'
+    }
+    resposne = requests.post(f'{BASE_URL}/tasks', json=new_task_data)
+    assert resposne.status_code == 200
