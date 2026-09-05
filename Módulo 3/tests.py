1110a8ca22782b947221db63cc1fea6b42e9aa97
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-05 13:13:10 -0400

    tesste de retorno de apenas uma task individual

--- Módulo 3/tests.py
+++ Módulo 3/tests.py
@@ -32,3 +32,15 @@ def test_get_tasks():
     response_json = response.json() # <- Esse método nos retorna o corpo da nossa estrutura json
     assert 'tasks' in response_json
     assert 'total_tasks' in response_json
+
+def test_get_task():
+
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f'{BASE_URL}/tasks/{task_id}')
+
+    assert response.status_code == 200
+    response_json = response.json()
+
+    assert task_id == response_json['id']
+
