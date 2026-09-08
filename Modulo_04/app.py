436e4540b601ec3c53ccdea70bba114288bdc1a0
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-08 11:38:21 -0400

    definindo a variavel que manipulará as informações no banco de dados e definindo uma chave secreta para manipulação dos dados em si

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456'
+db = SQLAlchemy(app) # Armazenando uma instância da classe SQLalchemy com o APP sendo o ponto de partida
+
