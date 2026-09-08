9c5f66dcc8a61ef2d02a1d1fd62f56903f39da0d
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-08 16:45:42 -0400

    ajustando importações dos arquivos para melhor diálogo entre eles

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # <- Caminho que o SQLAlquemy vai utilizar para conectar-se com nosso banco
 
-db = SQLAlchemy(app) # Armazenando uma instância da classe SQLalchemy com o APP sendo o ponto de partida
+db.init_app(app) # Armazenando uma instância da classe SQLalchemy com o APP sendo o ponto de partida
 
 if __name__ == '__main__':
     app.run()
