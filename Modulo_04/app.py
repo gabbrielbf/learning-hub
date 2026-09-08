59df4f79721578b07e20a40c45801daca82a88b1
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-08 16:27:40 -0400

    ajustando nomeclatura para eviar bugs ao tentar encontrar no terminal

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456'
-app.config['SQLALCHEMY_DATABASE-URI'] = 'sqlite:///database.db' # <- Caminho que o SQLAlquemy vai utilizar para conectar-se com nosso banco
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # <- Caminho que o SQLAlquemy vai utilizar para conectar-se com nosso banco
 
 db = SQLAlchemy(app) # Armazenando uma instância da classe SQLalchemy com o APP sendo o ponto de partida
 
