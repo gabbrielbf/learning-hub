b450d9b30642575ff52b977f33a4b1d17fe0e2c9
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-09 13:15:22 -0400

    voltando ao nome original para evitar conflitos com banco de dados

--- Modulo_04/models/user.py
+++ Modulo_04/models/user.py
@@ -5,7 +5,7 @@ class User(db.Model, UserMixin):
     # id[int], username[str], password[str]
     id = db.Column(db.Integer, primary_key=True) # Definindo o identificador com inteiro e criando a chave primária no qual serve como base
                                                  # para localizar determinado item dentro do banco
-    username = db.Column(db.String(80), nullable=False, unique=True) # Definindo o limite de caracteres em 80, 'nullable' define se
+    user_name = db.Column(db.String(80), nullable=False, unique=True) # Definindo o limite de caracteres em 80, 'nullable' define se
                                                                       # se conseguimos criar um usuário com espaço vazio ou não
                                                                       # e 'unique' nos permite criar APENAS um usuário com o mesmo nome
     password = db.Column(db.String(80), nullable=False)
