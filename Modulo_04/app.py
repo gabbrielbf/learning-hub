0a5b9932b3e2e6d09a14f61306c792a189a47dd5
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-09 11:43:29 -0400

    importando modulo de login e instânciando objeto para manipulação no login

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # <- Caminho que o SQLAlquemy vai utilizar para conectar-se com nosso banco
 
+login_manager = LoginManager()
 db.init_app(app) # Armazenando uma instância da classe SQLalchemy com o APP sendo o ponto de partida
+login_manager.init_app(app)
 
 if __name__ == '__main__':
     app.run()
