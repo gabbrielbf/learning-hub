cacbab64d81802daba5a946da9f742c19b7742f4
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-09 11:49:47 -0400

    lógica de login autenticado apenas se recebermos todas as informações criada

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager
@@ -10,6 +10,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' # <- Caminho que
 login_manager = LoginManager()
 db.init_app(app) # Armazenando uma instância da classe SQLalchemy com o APP sendo o ponto de partida
 login_manager.init_app(app)
+
+# View login
+@app.route('/login', methods=['POST'])
+def login():
+
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    # Lógica de login
+    if username and password:
+        pass
+
+    return jsonify({'message': 'Inválid credentials'}), 400
 
 if __name__ == '__main__':
     app.run()
