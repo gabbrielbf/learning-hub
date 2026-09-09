4753ca952a5001a3e6c4e0e55bd05fe9792fc688
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-09 17:59:40 -0400

    configurando lógica de logout

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456'
@@ -40,6 +40,12 @@ def login():
             return jsonify({'message': 'User authenticated successfully'})
 
     return jsonify({'message': 'Invalid credentials'}), 400
+
+@app.route('/logout', methods=['GET'])
+def logout():
+
+    logout_user()
+    return jsonify ({'message': 'Logout successful'})
 
 if __name__ == '__main__':
     app.run()
