f471e2e795488ea4d963157659c1332984195367
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-10 16:28:57 -0400

    corpo de busca para a deleção de X usuário criada

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -102,5 +102,16 @@ def update_user(id_user):
     
     return jsonify({'message': 'User not found'}), 404
 
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+
+    user = User.query.get(id_user)
+
+    if user:
+        return jsonify({'message': f'User [{user.username}] deleted successfully'})
+
+    return jsonify({'message': 'User not found'}), 404
+
 if __name__ == '__main__':
     app.run()
