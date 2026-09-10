48fb68af2619984ab6e335681ea7948e6482824e
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-10 13:06:03 -0400

    estruturando a busca por um usuário existente dentro do banco

--- Modulo_04/app.py
+++ Modulo_04/app.py
@@ -74,5 +74,16 @@ def create_user():
 
     return jsonify({'message': 'Invalid credentials'}), 401
 
+# Recuperando os dados de determinado usuário
+app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return {'username': user.username}
+
+    return jsonify({'messge': 'User not found'}), 404
+
 if __name__ == '__main__':
     app.run()
