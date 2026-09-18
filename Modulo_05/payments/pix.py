aa80b38a7c75dc6e50a540e17af02bd0097998a9
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-18 17:24:17 -0400

    criando classe responsável pelo método de pagamento PIX

---
+++ Modulo_05/payments/pix.py
@@ -0,0 +1,14 @@
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+
+        # Criando pagamento na instituição financeira, como não 
+        # faremos integração com nenhum banco, os dados serão gerados aqui
+        bank_payment_id = None
+
+        return {
+            'bank_payment_id': '',
+            'qr_code_path': ''
+        }
