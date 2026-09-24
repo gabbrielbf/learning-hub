94f6fb77619663979ad64bfc7b86929a799b11b4
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-09-24 11:51:30 -0400

    feat: emitindo mensagem de pagamento confirmando e retornando um template atualizado

--- Modulo_05/app.py
+++ Modulo_05/app.py
@@ -85,6 +85,7 @@ def pix_confirmation():
 
     payment.paid = True
     db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
 
     return jsonify({
         'message': 'The payment has been confirmed'
@@ -96,6 +97,9 @@ def pix_confirmation():
 def payment_pix_page(payment_id):
 
     payment = Payment.query.get(payment_id)
+
+    if payment.paid:
+        return render_template('confirmed_payment.html')
 
     return render_template('payment.html', 
                            payment_id=payment.id, 
